9b3a265ddcb19f6320c199544badb5e5dd97fe8c
Jørgen Sivesind <jsi@enonic.com>
2019-03-13 15:48:40 +0100

    #16 - Addding new pipeline properties

--- generate-changelog/src/main/java/com/enonic/xp/changelog/zenhub/issues/Pipeline.java
+++ generate-changelog/src/main/java/com/enonic/xp/changelog/zenhub/issues/Pipeline.java
@@ -4,14 +4,38 @@ class Pipeline
 {
     private String name;
 
+    private String workspace_id;
+
+    private String pipeline_id;
+
     public String getName ()
     {
         return name;
     }
 
-    public void setName (String name)
+    public void setName ( final String name)
     {
         this.name = name;
+    }
+
+    public String getWorkspace_id()
+    {
+        return workspace_id;
+    }
+
+    public void setWorkspace_id( final String workspace_id )
+    {
+        this.workspace_id = workspace_id;
+    }
+
+    public String getPipeline_id()
+    {
+        return pipeline_id;
+    }
+
+    public void setPipeline_id( final String pipeline_id )
+    {
+        this.pipeline_id = pipeline_id;
     }
 
     @Override
