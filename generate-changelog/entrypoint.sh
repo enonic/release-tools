51197278a4b3d57e399a4311b9157b96c44636c4
Anatol Sialitski <asi@enonic.com>
2020-07-31 09:59:47 +0300

    Fix gradle command for generating change logs if there no a previous release

--- generate-changelog/entrypoint.sh
+++ generate-changelog/entrypoint.sh
@@ -2,4 +2,9 @@
 
 DIR=${PWD}
 cd /
-/gradlew run --args="-s ${PREVIOS_RELEASE_TAG} -p $DIR -o ${DIR}/${OUTPUT_FILE}"
+if [[ -z "${PREVIOS_RELEASE_TAG}" ]]
+then
+      /gradlew run --args="-p $DIR -o ${DIR}/${OUTPUT_FILE}"
+else
+      /gradlew run --args="-s ${PREVIOS_RELEASE_TAG} -p $DIR -o ${DIR}/${OUTPUT_FILE}"
+fi
