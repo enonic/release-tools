ec661181c2267aa5cbc5054ae176379b1233b0c1
Anatol Sialitski <asi@enonic.com>
2020-09-08 17:49:10 +0300

    Automate SNAPSHOT version increase when released enonic/xp#8301

---
+++ scripts/workflow-updater/README.md
@@ -0,0 +1,12 @@
+# Workflow updater
+
+This script allows you to apply a [enonic-gradle.yml](https://github.com/enonic/.github/blob/master/workflow-templates/enonic-gradle.yml) file for all specified repositories.
+
+
+# Usage
+
+In order to execute this script you should to execute the following command:
+
+```
+node workflow-updater.js <BASIC_AUTH_TOKEN> <REPO_NAME_1> <REPO_NAME_2> 
+```
---
+++ scripts/workflow-updater/workflow-updater.js
@@ -0,0 +1,61 @@
+const {request} = require("@octokit/request");
+
+const AUTH_INDEX = 0;
+
+
+async function getFileInfo(owner, repo, path) {
+    return await request(`GET /repos/${owner}/${repo}/contents/${path}`);
+}
+
+async function getShaIfFileExists(owner, repo, path) {
+    try {
+        const res = await getFileInfo(owner, repo, path);
+
+        return res.data.sha;
+    } catch (e) {
+        return null;
+    }
+}
+
+async function updateWorkflow(owner, repo, path, content) {
+    let data = {
+        message: 'Update workflow',
+        content: content
+    };
+
+    let fileSHA = await getShaIfFileExists(owner, repo, path);
+
+    if (fileSHA != null) {
+        data.sha = fileSHA;
+    }
+
+    return await request({
+        headers: {
+            Authorization: `Basic ${process.argv.slice(2)[AUTH_INDEX]}`
+        },
+        method: 'PUT',
+        url: `/repos/${owner}/${repo}/contents/${path}`,
+        data: data
+    });
+}
+
+function execute() {
+    getFileInfo('enonic', '.github', 'workflow-templates/enonic-gradle.yml')
+        .then(template => {
+            let repos = process.argv.slice(3);
+
+            repos.forEach(async (repo) => {
+                await updateWorkflow('anatol-sialitski', repo, '.github/workflows/enonic-gradle.yml', template.data.content)
+                    .then(res => {
+                        console.log(`Repo "${repo}" was updated`);
+                    })
+                    .catch(err => {
+                        console.log(JSON.stringify(err, null, 4));
+                        console.log(`Repo "${repo}" was not updated`);
+                    });
+            });
+
+        }).catch(err => console.log(JSON.stringify(err, null, 4)));
+}
+
+execute();
